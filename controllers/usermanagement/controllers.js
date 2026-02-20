4c7a46e838c610e6fdeab87a0dd38145cb8a0801
Abishek <abish.yoho@gmail.com>
2026-02-20 09:43:14 +0530

    Correction

--- controllers/usermanagement/controllers.js
+++ controllers/usermanagement/controllers.js
@@ -4,12 +4,10 @@ import userSchema from "../../models/users/userSchema.js";
 export const createUser = async (req, res) => {
     try {
         const { role_id, full_name, email, mobile, status } = req.body;
-
         const role = await roleSchema.findById(role_id);
         if (!role) {
             return res.status(404).json({ message: "Role not found" });
         }
-
         const user = await userSchema.create({
             role_id,
             full_name,
@@ -17,7 +15,6 @@ export const createUser = async (req, res) => {
             mobile,
             status,
         });
-
         res.status(201).json({
             message: "User created successfully",
             user,
@@ -37,34 +34,27 @@ export const getUsers = async (req, res) => {
 
 export const getUserById = async (req, res) => {
     const user = await userSchema.findById(req.params.id).populate("role_id");
-
     if (!user)
         return res.status(404).json({ message: "User not found" });
-
     res.json(user);
 };
 
 export const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
-
         const user = await userSchema.findById(id);
-
         if (!user)
             return res.status(404).json({ message: "User not found" });
         if (req.body.role_id) {
             const role = await roleSchema.findById(req.body.role_id);
-
             if (!role)
                 return res.status(404).json({ message: "Role not found" });
         }
-
         const updatedUser = await userSchema.findByIdAndUpdate(
             id,
             req.body,
             { new: true }
         ).populate("role_id");
-
         res.json({
             message: "User updated successfully",
             user: updatedUser,
@@ -76,15 +66,11 @@ export const updateUser = async (req, res) => {
 
 export const deactivateUser = async (req, res) => {
     const { id } = req.params;
-
     const user = await userSchema.findById(id);
-
     if (!user)
         return res.status(404).json({ message: "User not found" });
-
     user.status = "Deactivated";
     await user.save();
-
     res.json({
         message: "User deactivated successfully",
     });
